e6d33974bfd3f68f450edf27cc39512764a5ac34
Amandeep <AmandeepSingh_K@infosys.com>
2020-07-24 22:02:59 +0530

    working

--- file-package.groovy
+++ file-package.groovy
@@ -3,8 +3,9 @@ node{
     def server = Artifactory.server "CHDSEZ296396L"
     // Create an Artifactory Maven instance.
     def rtMaven = Artifactory.newMavenBuild()
-    def buildInfo
 
+    def buildInfo
+             
 		    stage('Artifactory configuration') {
 				// Tool name from Jenkins configuration
 				rtMaven.tool = "maven_3_6_3"
@@ -13,12 +14,23 @@ node{
 				rtMaven.resolver releaseRepo:'libs-release', snapshotRepo:'libs-snapshot', server: server
 			}
 
+			stage('Clone sources') {
+                script{
+                     
+                     git(
+					       url: 'https://github.com/Akahlon2003/Package.git',
+					       credentialsId: 'Git-akahlon2003',
+					       branch: 'master'
+					     )
+                }
+            }
+            
 			stage('Maven build') {
-				//buildInfo = rtMaven.run pom: 'pom.xml', goals: 'clean install'
+				buildInfo = rtMaven.run pom: 'pom.xml', goals: 'clean install'
 			}
 
 			stage('Publish build info') {
-				//server.publishBuildInfo buildInfo
+				server.publishBuildInfo buildInfo
 			}
 
 }
